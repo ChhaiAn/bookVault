8eaaeee243aa40401115677e1029937fe91662a1
Chhai An <Chhai@Chhais-MacBook-Pro.local>
2018-09-25 14:23:02 -0700

    edited some layout

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { SignUpComponent } from "./components/sign-up/sign-up.component";
-import { StudentComponent } from "./components/student/student.component"
+import { StudentComponent } from "./components/student/student.component";
+import { CartComponent } from "./components/cart/cart.component";
 
 const routes: Routes = [
   { path: "", component: SignUpComponent },
-  {path:"studentDashboard" , component: StudentComponent}
-
+  { path: "studentDashboard", component: StudentComponent },
+  { path: "cart", component: CartComponent }
 ];
 
 @NgModule({
